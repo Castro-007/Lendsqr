e2cbf8cfc1583a9fdbb97824605f229551153875
Castro-007 <sammythompson202@gmail.com>
2025-08-18 00:02:47 +0100

    added server and allowedhost

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    allowedHosts: ['https://lendsqr-1ovd.onrender.com'],
+    port: process.env.PORT || 5173,
+  },
 })
